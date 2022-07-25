22b08d7d8c9bb314a2914c1bb40feeaaf532a5cf
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-07-25 23:25:12 +0000

    assert with colors

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅ Assertion Passed:" + actual + " === " + expected);
+  } else {
+    console.log("🔴 🔴 🔴 Assertion Faild:" + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual(3, 4);
