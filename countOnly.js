113eb75adc40bd5726a9cd256a152bc943daaf7f
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-07-27 19:50:43 +0000

    countOnly function

---
+++ countOnly.js
@@ -0,0 +1,55 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Faild: ${actual} !==  ${expected}`);
+  }
+};
+
+const countOnly = function (allitems, itemsTocount) {
+  let result = {};
+  for (const item of allitems) {
+    if (itemsTocount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+
+  return result;
+};
+
+/// Test data
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+// // to get the value
+// for (let xxx in object) {
+//   console.log(object[xxx]);
+// }
+
+// Object.values(object);
