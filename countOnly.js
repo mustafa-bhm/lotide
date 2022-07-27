21bc83c04bef256acadac45946990df8ac2a30fa
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-07-27 19:52:25 +0000

    changes

--- countOnly.js
+++ countOnly.js
@@ -46,10 +46,3 @@ assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
-
-// // to get the value
-// for (let xxx in object) {
-//   console.log(object[xxx]);
-// }
-
-// Object.values(object);
