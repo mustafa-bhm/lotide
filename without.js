6639f77311aee68dd6fc58e1b008805fb07663b1
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-07-28 00:41:45 +0000

    updates

--- without.js
+++ without.js
@@ -1,17 +1,12 @@
-const assertArraysEqual = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+const assertArraysEqual = function (arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
     return console.log(`🔴 🔴 🔴 Assertion Faild: ${arr1} !==  ${arr2}`);
   } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return console.log(`🔴 🔴 🔴 Assertion Faild: ${arr1} !==  ${arr2}`);
-      }
-    }
     return console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} ===  ${arr2}`);
   }
 };
 
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   } else {
@@ -24,24 +19,28 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-// a function to remove unwanted elements from an arry
-
-const without = function(source, itemsToRemove) {
-  for (let i = 0; i < itemsToRemove.length; i++) {
-    if (source[i] === itemsToRemove[i]) {
-      source.splice(i, 1);
+const without = function (source, itemsToRemove) {
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        source.splice(i, 1);
+      }
     }
   }
   return source;
 };
 
 // TEST CODE
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without([2, 3, 1], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
 const words = ["hello", "world", "lighthouse"];
-without(["hello", "world", "lighthouse"], ["lighthouse"]);
+console.log(without(["hello", "world", "lighthouse"], ["lighthouse"]));
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 without([1, 2, 3], [1, 2]); // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]);
+assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), [
+  "hello",
+  "world",
+]);
