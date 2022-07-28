d025d3a273e31ccd6c595785c5075862fb2f1b60
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-07-28 21:30:49 +0000

    map function

---
+++ map.js
@@ -0,0 +1,40 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2) === false) {
+    return console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  } else {
+    return console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+const results2 = map(words, (word) => word);
+
+console.log("final output for result", results1);
+
+//// Test data
+
+assertArraysEqual(words, results1); // => should faild
+assertArraysEqual(words, results2); // => should pass
