3dcb6de31a174639f117b2f6e2b1ac29a7667a24
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-01 14:38:31 +0000

    Refactoring

--- tail.js
+++ tail.js
@@ -1,14 +1,4 @@
-const head = function (arr) {
-  let firstElm = arr[0];
-  return firstElm;
-};
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 🔴 🔴 Assertion Faild: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual");
 
 const tail = function (arr) {
   return arr.slice(1);
@@ -18,3 +8,5 @@ const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2);
 assertEqual(result[0], "Lighthouse");
 assertEqual(result[1], "Labs");
+
+module.exports = tail;
