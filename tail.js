3f3bbac51bb784b8a4eaa321df3847651b5a6ac0
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-01 14:39:38 +0000

    Refactoring

--- tail.js
+++ tail.js
@@ -4,9 +4,4 @@ const tail = function (arr) {
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
 module.exports = tail;
