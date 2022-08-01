d837110e7af6a374f6b98034b455d16f75689168
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-01 15:11:43 +0000

    Refactoring

--- middle.js
+++ middle.js
@@ -1,31 +1,10 @@
-// Test /assertion function
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2) === false) {
-    return console.log(`🛑 🛑 🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  } else {
-    return console.log(`✅ ✅ ✅ Assertion Passed: ${arr1} === ${arr2}`);
-  }
-};
-
-/// Actual function
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function (arr) {
   let result = [];
+  // to get the index of the mid number
   let mid = arr[Math.floor(arr.length / 2)];
-  // let midEven = arr[Math.floor(arr.length / 2) + 1];
 
   if (arr.length <= 2) {
     return result;
@@ -40,13 +19,4 @@ const middle = function (arr) {
   return result;
 };
 
-/// Test code ///////
-
-/// odd arrays
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-/// even arrays
-
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-middle([1, 2, 3, 4, 5, 6, 7, 8]); // => [4, 5]
+module.exports = middle;
