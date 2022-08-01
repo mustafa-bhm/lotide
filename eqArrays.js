9a5097831d09c095dcaf75b873a1c989d6959fc1
mustafa-bhm <mustafa.bhm3@gmail.com>
2022-08-01 14:50:14 +0000

    Refactoring

--- eqArrays.js
+++ eqArrays.js
@@ -13,7 +13,4 @@ const eqArrays = function (arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
 module.exports = eqArrays;
